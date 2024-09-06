318f6a5c64f241cb6a04e681898fbc4b001848eb
Alister <a.enikeev@mirador.software>
2024-09-06 14:26:36 +0400

    add dark mode

--- script.js
+++ script.js
@@ -32,3 +32,15 @@ function createKeyValuePair() {
   })
   return element
 }
+
+const darkModeToggle = document.getElementById('darkModeToggle')
+darkModeToggle.addEventListener('click', () => {
+  if (document.body.classList.contains('dark-mode')) {
+    document.body.classList.remove('dark-mode')
+    document.body.classList.add('light-mode')
+  } else {
+    document.body.classList.remove('light-mode')
+    document.body.classList.add('dark-mode')
+  }
+})
+document.body.classList.add('dark-mode')
